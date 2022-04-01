47951017dd42199f9fe36257d95a942cc096e7a9
bruno teixeira <btex88@gmail.com>
2022-04-01 19:09:43 -0300

    feat(not-found): add not found page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { MdHome } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import notFound from '../images/not-found.svg';
 
 export default function NotFound() {
-  return <div />;
+  const navigate = useNavigate();
+  return (
+    <div
+      className="w-full h-screen flex flex-col flex-nowrap items-center justify-start
+      mt-24"
+    >
+      <MdHome
+        size="45"
+        className="absolute top-4 fill-zinc-600 cursor-pointer"
+        onClick={() => navigate('/')}
+      />
+      <span className="font_quicksand text-3xl sm:text-4xl md:text-5xl text-center mb-12">
+        Página não encontrada!
+      </span>
+      <img src={notFound} alt="not found" />
+    </div>
+  );
 }
